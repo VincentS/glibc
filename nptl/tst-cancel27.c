b92a4eec24afc1851b2c5a072ec40cf1462babee
Adhemerval Zanella <adhemerval.zanella@linaro.com>
2015-09-21 15:54:04 -0700

    nptl: Add NPTL cases for cancellation failures cases
    This patch pthread cancellation tests to check for failures cases
    wherer the syscall cancel wrapper should both set the error and
    the errno values.
    
    	* nptl/tst-cancel26.c: New file.
    	* nptl/tst-cancel27.c: Likewise.

---
+++ nptl/tst-cancel27.c
@@ -0,0 +1,70 @@
+/* Copyright (C) 2015 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static void *
+tf (void *arg)
+{
+  int e, r;
+
+  errno = 0;
+
+  pthread_setcancelstate (PTHREAD_CANCEL_DISABLE, NULL);
+
+  /* This is a cancellation point, but we should not be cancelled.  */
+  r = write (-1, 0, 0);
+  e = errno;
+
+  /* Check that the cancelling syscall wrapper has handled the error correctly.  */
+  if (r != -1 || errno != EBADF)
+    {
+      printf ("write returned %d, errno %d\n", r, e);
+      exit (1);
+    }
+
+  return NULL;
+}
+
+static int
+do_test (void)
+{
+  pthread_t th;
+
+  if (pthread_create (&th, NULL, tf, NULL) != 0)
+    {
+      puts ("create failed");
+      exit (1);
+    }
+
+  void *r;
+  if (pthread_join (th, &r) != 0)
+    {
+      puts ("join failed");
+      exit (1);
+    }
+
+  return 0;
+}
+
+#define TEST_FUNCTION do_test ()
+#include "../test-skeleton.c"
